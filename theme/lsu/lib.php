a7034d9a4b5c95b256f0f798cb8d58f1c269daf9
Adam Zapletal <adamzap@gmail.com>
2011-08-12 13:37:05 -0500

    Added lsu_theme v0.5

---
+++ theme/lsu/lib.php
@@ -0,0 +1,167 @@
+<?php
+
+/**
+ * Makes our changes to the CSS
+ *
+ * @param string $css
+ * @param theme_config $theme
+ * @return string
+ */
+function lsu_process_css($css, $theme) {
+
+    // Set the link color
+    if (!empty($theme->settings->linkcolor)) {
+        $linkcolor = $theme->settings->linkcolor;
+    } else {
+        $linkcolor = null;
+    }
+    $css = lsu_set_linkcolor($css, $linkcolor);
+
+    // Set the hover color
+    if (!empty($theme->settings->hovercolor)) {
+        $hovercolor = $theme->settings->hovercolor;
+    } else {
+        $hovercolor = null;
+    }
+    $css = lsu_set_hovercolor($css, $hovercolor);
+
+    // Set the header background color
+    if (!empty($theme->settings->header_bgcolor)) {
+        $header_bgcolor = $theme->settings->header_bgcolor;
+    } else {
+        $header_bgcolor = null;
+    }
+    $css = lsu_set_header_bgcolor($css, $header_bgcolor);
+
+    // Set the header background image
+    if (!empty($theme->settings->header_background_image)) {
+        $header_background_image = $theme->settings->header_background_image;
+    } else {
+        $header_background_image = null;
+    }
+    $css = lsu_set_header_background_image($css, $header_background_image);
+
+    // Set the region width
+    if (!empty($theme->settings->regionwidth)) {
+        $regionwidth = $theme->settings->regionwidth;
+    } else {
+        $regionwidth = null;
+    }
+    $css = lsu_set_regionwidth($css, $regionwidth);
+
+    // Set the custom CSS
+    if (!empty($theme->settings->customcss)) {
+        $customcss = $theme->settings->customcss;
+    } else {
+        $customcss = null;
+    }
+    $css = lsu_set_customcss($css, $customcss);
+
+    // Return the CSS
+    return $css;
+}
+
+/**
+ * Sets the link color variable in CSS
+ *
+ * @param string $css
+ * @param mixed $linkcolor
+ * @return string
+ */
+function lsu_set_linkcolor($css, $linkcolor) {
+    $tag = '[[setting:linkcolor]]';
+    $replacement = $linkcolor;
+    if (is_null($replacement)) {
+        $replacement = '#461d7c';
+    }
+    $css = str_replace($tag, $replacement, $css);
+    return $css;
+}
+
+/**
+ * Sets the link hover color variable in CSS
+ *
+ * @param string $css
+ * @param mixed $hovercolor
+ * @return string
+ */
+function lsu_set_hovercolor($css, $hovercolor) {
+    $tag = '[[setting:hovercolor]]';
+    $replacement = $hovercolor;
+    if (is_null($replacement)) {
+        $replacement = '#fdd023';
+    }
+    $css = str_replace($tag, $replacement, $css);
+    return $css;
+}
+
+/**
+ * Sets the header background color variable in CSS
+ *
+ * @param string $css
+ * @param mixed $header_bgcolor
+ * @return string
+ */
+function lsu_set_header_bgcolor($css, $header_bgcolor) {
+    $tag = '[[setting:header_bgcolor]]';
+    $replacement = $header_bgcolor;
+    if (is_null($replacement)) {
+        $replacement = '#461d7c';
+    }
+    $css = str_replace($tag, $replacement, $css);
+    return $css;
+}
+
+/**
+ * Sets the header background image variable in CSS
+ *
+ * @param string $css
+ * @param mixed $header_background_image
+ * @return string
+ */
+function lsu_set_header_background_image($css, $header_background_image) {
+    $tag = '[[setting:header_background_image]]';
+    $replacement = $header_background_image;
+    if (is_null($replacement)) {
+        $replacement = 'lsu/pix/lsu_bg_logo.png';
+    }
+    $css = str_replace($tag, $replacement, $css);
+    return $css;
+}
+
+/**
+ * Sets the region width variable in CSS
+ *
+ * @param string $css
+ * @param mixed $regionwidth
+ * @return string
+ */
+ 
+function lsu_set_regionwidth($css, $regionwidth) {
+    $tag = '[[setting:regionwidth]]';
+    $doubletag = '[[setting:regionwidthdouble]]';
+    $replacement = $regionwidth;
+    if (is_null($replacement)) {
+        $replacement = 250;
+    }
+    $css = str_replace($tag, $replacement.'px', $css);
+    $css = str_replace($doubletag, ($replacement*2).'px', $css);
+    return $css;
+}
+
+/**
+ * Sets the custom css variable in CSS
+ *
+ * @param string $css
+ * @param mixed $customcss
+ * @return string
+ */
+function lsu_set_customcss($css, $customcss) {
+    $tag = '[[setting:customcss]]';
+    $replacement = $customcss;
+    if (is_null($replacement)) {
+        $replacement = '';
+    }
+    $css = str_replace($tag, $replacement, $css);
+    return $css;
+}
---
+++ theme/lsu/settings.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Settings for the lsu theme
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+if ($ADMIN->fulltree) {
+    // Logo file setting
+    $name = 'theme_lsu/logo';
+    $title = get_string('logo','theme_lsu');
+    $description = get_string('logodesc', 'theme_lsu');
+    $default = $CFG->wwwroot . 'theme/lsu/pix/lsu_logo.png';
+    $setting = new admin_setting_configtext($name, $title, $description, '', PARAM_URL);
+    $settings->add($setting);
+
+    // Header background image file setting
+    $name = 'theme_lsu/header_background_image';
+    $title = get_string('background_image','theme_lsu');
+    $description = get_string('background_image_desc', 'theme_lsu');
+    $default = $CFG->wwwroot . 'theme/lsu/pix/lsu_bg_logo.png';
+    $setting = new admin_setting_configtext($name, $title, $description, '', PARAM_URL);
+    $settings->add($setting);
+
+    // Header background color setting
+    $name = 'theme_lsu/header_bgcolor';
+    $title = get_string('header_bgcolor','theme_lsu');
+    $description = get_string('header_bgcolor_desc', 'theme_lsu');
+    $default = '#461d7c';
+    //$previewconfig = array('selector'=>'.block .content', 'style'=>'header_bgcolor');
+    $setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);
+    $settings->add($setting);
+
+    // Tagline setting
+    $name = 'theme_lsu/tagline';
+    $title = get_string('tagline','theme_lsu');
+    $description = get_string('taglinedesc', 'theme_lsu');
+    $setting = new admin_setting_configtext($name, $title, $description, '');
+    $settings->add($setting);
+
+    // Link colour setting
+    $name = 'theme_lsu/linkcolor';
+    $title = get_string('linkcolor','theme_lsu');
+    $description = get_string('linkcolordesc', 'theme_lsu');
+    $default = '#80729d';
+    $previewconfig = array('selector'=>'.block .content', 'style'=>'linkcolor');
+    $setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);
+    $settings->add($setting);
+
+    // Link Hover colour setting
+    $name = 'theme_lsu/hovercolor';
+    $title = get_string('hovercolor','theme_lsu');
+    $description = get_string('hovercolordesc', 'theme_lsu');
+    $default = '#fdd023';
+    $previewconfig = array('selector'=>'.block .content', 'style'=>'hovercolor');
+    $setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);
+    $settings->add($setting);
+
+    // Block region width
+    $name = 'theme_lsu/regionwidth';
+    $title = get_string('regionwidth','theme_lsu');
+    $description = get_string('regionwidthdesc', 'theme_lsu');
+    $default = 250;
+    $choices = array(180=>'180px', 190=>'190px', 200=>'200px', 210=>'210px', 220=>'220px', 240=>'240px', 250=>'250px');
+    $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+    $settings->add($setting);
+
+    // Custom CSS file
+    $name = 'theme_lsu/customcss';
+    $title = get_string('customcss','theme_lsu');
+    $description = get_string('customcssdesc', 'theme_lsu');
+    $setting = new admin_setting_configtextarea($name, $title, $description, '');
+    $settings->add($setting);
+}
