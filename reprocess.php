0ea6e313cd0c91a680196852aed89aab2d29fd69
Philip Cali <philip.cali@gmail.com>
2012-01-05 15:18:15 -0600

    Amending the reprocess link

--- reprocess.php
+++ reprocess.php
@@ -5,22 +5,20 @@
 require_once 'config.php';
 require_once $CFG->dirroot . '/enrol/ues/publiclib.php';
 
-ues::requrie_daos();
-
-header("Content-Type: text/plain");
+ues::require_daos();
 
 if (!is_siteadmin($USER->id)) {
     echo "You don'y exist. Go away.";
     exit;
 }
 
-
 $courseid = required_param('id', PARAM_INT);
 $course = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST);
 
+header("Content-Type: text/plain");
+
 $sections = ues_section::from_course($course);
 
-ues::unenroll_users($sections, false);
 ues::enroll_users($sections, false);
 
 echo "Done";
