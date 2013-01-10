6490708c512ff3cdcfaa35a5c24e871283efca6b
Jason Peak <jpeak5@lsu.edu>
2013-01-10 13:09:32 -0600

    adds link to kaltura report under the course > reports nav item

--- local/kaltura/lib.php
+++ local/kaltura/lib.php
@@ -30,8 +30,8 @@ require_once(dirname(__FILE__) . '/locallib.php');
  */
 function kaltura_extends_navigation($navigation) {
 
-    global $USER, $PAGE, $SITE;
-
+    global $USER, $PAGE, $SITE, $COURSE;
+//print_r($navigation);
     // Finds courses where the user has this capabiltiy
     $courses = get_user_capability_course('local/kaltura:view_report', null, true, 'shortname', 'shortname ASC');
 
@@ -45,7 +45,7 @@ function kaltura_extends_navigation($navigation) {
 
     //get a reference to the 'Site Pages' root nav item
     $node_home = $navigation->get('1');
-    $reports_home = $node_home->find(6);
+    $reports_home = $node_home->find(6,  navigation_node::TYPE_UNKNOWN);
 
     $report_text = get_string('kaltura_course_reports', 'local_kaltura');
 
@@ -55,13 +55,29 @@ function kaltura_extends_navigation($navigation) {
 
     $current_course = $PAGE->course->id;
     $i   = 5;
-
+    
+        
+        $courses_node = $navigation->get('mycourses');
+        
     foreach ($courses as $key => $course) {
 
         if ($SITE->id == $course->id) {
+            
             $i++;
             continue;
         }
+if($COURSE->id == $course->id){
+    $courses_node = $navigation->get('mycourses');
+	if($courses_node){
+		if($COURSE->id == $course->id){
+		$course_node = $courses_node->find($COURSE->id, navigation_node::TYPE_UNKNOWN);
+	    	$reports_node = $course_node->find(1,navigation_node::TYPE_UNKNOWN);
+		$reports_node->add('Kaltura Viewership', new moodle_url('/local/kaltura/reports.php',
+                                                        array('courseid' => $course->id)),
+                                                        navigation_node::NODETYPE_LEAF, $course_name, 'kal_reports_course' . $i);
+	    }
+	}
+}
 
         $course_name = format_string($course->shortname);
         $node_reports->add($course_name, new moodle_url('/local/kaltura/reports.php',
@@ -70,5 +86,7 @@ function kaltura_extends_navigation($navigation) {
 
         $i++;
     }
+    
+
 }
 
