031160f7f22eac28139aeb3e48c59cd79bbb27d0
Jason Peak <jpeak5@lsu.edu>
2013-01-29 17:00:54 -0600

    removes the stupidly inefficient reports link generation; next to check capability before doing anything at all with reports links...

--- local/kaltura/lib.php
+++ local/kaltura/lib.php
@@ -66,24 +66,24 @@ function kaltura_extends_navigation($navigation) {
             $i++;
             continue;
         }
-if($COURSE->id == $course->id){
-    $courses_node = $navigation->get('mycourses');
-	if($courses_node){
-		if($COURSE->id == $course->id){
-		$course_node = $courses_node->find($COURSE->id, navigation_node::TYPE_UNKNOWN);
-	    	$reports_node = $course_node->find(1,navigation_node::TYPE_UNKNOWN);
-		$reports_node->add('Kaltura Viewership', new moodle_url('/local/kaltura/reports.php',
-                                                        array('courseid' => $course->id)),
-                                                        navigation_node::NODETYPE_LEAF, $course_name, 'kal_reports_course' . $i);
-	    }
-	}
-}
+        if($COURSE->id == $course->id){
+            $courses_node = $navigation->get('mycourses');
+                if($courses_node){
+                        if($COURSE->id == $course->id){
+                        $course_node = $courses_node->find($COURSE->id, navigation_node::TYPE_UNKNOWN);
+                        $reports_node = $course_node->find(1,navigation_node::TYPE_UNKNOWN);
+                        $reports_node->add('Kaltura Viewership', new moodle_url('/local/kaltura/reports.php',
+                                                                array('courseid' => $course->id)),
+                                                                navigation_node::NODETYPE_LEAF, $course_name, 'kal_reports_course' . $i);
+                    }
+                }
+        }
 
-        $course_name = format_string($course->shortname);
-        $node_reports->add($course_name, new moodle_url('/local/kaltura/reports.php',
-                                                        array('courseid' => $course->id)),
-                                                        navigation_node::NODETYPE_LEAF, $course_name, 'kal_reports_course' . $i);
-
+//        $course_name = format_string($course->shortname);
+//        $node_reports->add($course_name, new moodle_url('/local/kaltura/reports.php',
+//                                                        array('courseid' => $course->id)),
+//                                                        navigation_node::NODETYPE_LEAF, $course_name, 'kal_reports_course' . $i);
+ 
         $i++;
     }
     
