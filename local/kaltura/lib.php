f36acf1bcd8fe87da515ef7cade4ce9c5e275531
Jason Peak <jpeak5@lsu.edu>
2013-01-30 09:32:28 -0600

    replaces Kaltura's use of inefficient methods to generate links for reports
    find the report link for a given course under the reports nav item WHEN VIEWING the course...

--- local/kaltura/lib.php
+++ local/kaltura/lib.php
@@ -30,16 +30,13 @@ require_once(dirname(__FILE__) . '/locallib.php');
  */
 function kaltura_extends_navigation($navigation) {
 
-    global $USER, $PAGE, $SITE, $COURSE;
-//print_r($navigation);
-    // Finds courses where the user has this capabiltiy
-    $courses = get_user_capability_course('local/kaltura:view_report', null, true, 'shortname', 'shortname ASC');
-
+    global $COURSE;
+    
+    $context = get_context_instance(CONTEXT_COURSE, $COURSE->id, MUST_EXIST);
     if (!isloggedin()) {
         return '';
     }
-
-    if (empty($courses)) {
+    if(!has_capability('local/kaltura:view_report', $context)){
         return '';
     }
 
@@ -53,40 +50,21 @@ function kaltura_extends_navigation($navigation) {
         $node_reports = $reports_home->add($report_text, null, 70, $report_text, 'kal_reports');
     }
 
-    $current_course = $PAGE->course->id;
-    $i   = 5;
+    $courses_node = $navigation->get('mycourses');
     
-        
-        $courses_node = $navigation->get('mycourses');
-        
-    foreach ($courses as $key => $course) {
+    if($courses_node){
 
-        if ($SITE->id == $course->id) {
-            
-            $i++;
-            continue;
-        }
-        if($COURSE->id == $course->id){
-            $courses_node = $navigation->get('mycourses');
-                if($courses_node){
-                        if($COURSE->id == $course->id){
-                        $course_node = $courses_node->find($COURSE->id, navigation_node::TYPE_UNKNOWN);
-                        $reports_node = $course_node->find(1,navigation_node::TYPE_UNKNOWN);
-                        $reports_node->add('Kaltura Viewership', new moodle_url('/local/kaltura/reports.php',
-                                                                array('courseid' => $course->id)),
-                                                                navigation_node::NODETYPE_LEAF, $course_name, 'kal_reports_course' . $i);
-                    }
-                }
-        }
-
-//        $course_name = format_string($course->shortname);
-//        $node_reports->add($course_name, new moodle_url('/local/kaltura/reports.php',
-//                                                        array('courseid' => $course->id)),
-//                                                        navigation_node::NODETYPE_LEAF, $course_name, 'kal_reports_course' . $i);
- 
-        $i++;
+        $course_node = $courses_node->find($COURSE->id, navigation_node::TYPE_UNKNOWN);
+        $reports_node = $course_node->find(1,navigation_node::TYPE_UNKNOWN);
+        $reports_node->add(
+                'Kaltura Viewership', 
+                new moodle_url('/local/kaltura/reports.php',array('courseid' => $COURSE->id)),
+                navigation_node::NODETYPE_LEAF, 
+                format_string($COURSE->shortname), 
+                'kal_reports_course'
+                );
     }
-    
+
 
 }
 
